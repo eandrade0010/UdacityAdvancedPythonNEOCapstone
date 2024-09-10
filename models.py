cfcd8c74bea91b0a14ab2e730274f1fc08987fa7
Eduardo <eandrade.0010@gmail.com>
2024-09-09 18:55:51 -0700

    Deleted instructionary comments

--- models.py
+++ models.py
@@ -105,9 +105,6 @@ class CloseApproach:
         formatted string that can be used in human-readable representations and
         in serialization to CSV and JSON files.
         """
-        # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
-        # build a formatted representation of the approach time.
-        # TODO: Use self.designation and self.name to build a fullname for this object.
         return datetime_to_str(self.time)
 
     @property
