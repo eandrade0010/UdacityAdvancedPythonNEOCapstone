b569e7d54132a1fd5e464a7a30aaaf88a3f5ef82
Eduardo <eandrade.0010@gmail.com>
2024-09-11 09:53:35 -0700

    Implemented writing to csv and json files

--- write.py
+++ write.py
@@ -28,7 +28,15 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames)
+        writer.writeheader()
+        if results:
+            for result in results:
+                writer.writerow({'datetime_utc': result.time_str, 'distance_au': result.distance,
+                                 'velocity_km_s': result.velocity, 'designation': result.neo.designation,
+                                 'name': result.neo.name, 'diameter_km': result.neo.diameter,
+                                 'potentially_hazardous': result.neo.hazardous})
 
 
 def write_to_json(results, filename):
@@ -42,4 +50,15 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+
+    approach_keys = ['datetime_utc', 'distance_au', 'velocity_km_s']
+    neo_keys = ['designation', 'name', 'diameter_km', 'potentially_hazardous']
+    data = []
+    for result in results:
+        entry = {k: v for k, v in zip(approach_keys, [result.time_str, result.distance, result.velocity])}
+        entry['neo'] = {k: v for k, v in zip(neo_keys, [result.neo.designation, result.neo.name, result.neo.diameter,
+                                                        result.neo.hazardous])}
+        data.append(entry)
+
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile, indent=2)
