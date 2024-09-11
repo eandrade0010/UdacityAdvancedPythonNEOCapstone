597525708e1c7148b17c20811b8ca11c24e8dc62
Eduardo <eandrade.0010@gmail.com>
2024-09-10 21:25:36 -0700

    Successfully implemented filters

--- filters.py
+++ filters.py
@@ -72,6 +72,36 @@ class AttributeFilter:
         return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
 
 
+class DateFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.time.date()
+
+
+class DistanceFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.distance
+
+
+class VelocityFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.velocity
+
+
+class DiameterFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.diameter
+
+
+class HazardFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.hazardous
+
+
 def create_filters(
         date=None, start_date=None, end_date=None,
         distance_min=None, distance_max=None,
@@ -108,8 +138,19 @@ def create_filters(
     :param hazardous: Whether the NEO of a matching `CloseApproach` is potentially hazardous.
     :return: A collection of filters for use with `query`.
     """
-    # TODO: Decide how you will represent your filters.
-    return ()
+    f_date = DateFilter(operator.eq, date) if date else None
+    f_start_date = DateFilter(operator.ge, start_date) if start_date else None
+    f_end_date = DateFilter(operator.le, end_date) if end_date else None
+    f_distance_min = DistanceFilter(operator.ge, distance_min) if distance_min else None
+    f_distance_max = DistanceFilter(operator.le, distance_max) if distance_max else None
+    f_velocity_min = VelocityFilter(operator.ge, velocity_min) if velocity_min else None
+    f_velocity_max = VelocityFilter(operator.le, velocity_max) if velocity_max else None
+    f_diameter_min = DiameterFilter(operator.ge, diameter_min) if diameter_min else None
+    f_diameter_max = DiameterFilter(operator.le, diameter_max) if diameter_max else None
+    f_hazardous = HazardFilter(operator.eq, hazardous) if hazardous is not None else None
+
+    return (f_date, f_start_date, f_end_date, f_distance_min, f_distance_max, f_velocity_min, f_velocity_max,
+            f_diameter_min, f_diameter_max, f_hazardous)
 
 
 def limit(iterator, n=None):
@@ -122,4 +163,4 @@ def limit(iterator, n=None):
     :yield: The first (at most) `n` values from the iterator.
     """
     # TODO: Produce at most `n` values from the given iterator.
-    return iterator
+    return iterator[:n-1]
